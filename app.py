ded5e77ee55718e7222ff74bf59c3119f2745ecf
dmarmolejo510 <159586369+dmarmolejo510@users.noreply.github.com>
2024-02-10 08:24:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,15 @@ def YMS_root():
             for K in request.form.keys():
                 Datos[K] = escape(request.form[K]).striptags()
             return render_template_string(YMS.Direccionar(Datos))
-        
+@app.route("/YMS/Container_Control",methods=['GET','POST'])
+def YMS_Contenedores():
+    if "IDu" not in session:
+        return render_template_string("<script>window.location= '"+str(request.url_root)+"';</script>")
+    else:
+        if request.method == "GET":
+            return render_template_string(YMS_Contenedores.Inicio())
+        else:
+            Datos = {}
+            for K in request.form.keys():
+                Datos[K] = escape(request.form[K]).striptags()
+            return render_template_string(YMS_Contenedores.Direccionar(Datos))
