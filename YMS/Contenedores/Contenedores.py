1ffa141411c9f03f2dc32793f00a20c1b0046cd1
dmarmolejo510 <159586369+dmarmolejo510@users.noreply.github.com>
2024-02-11 12:21:26 -0600

    Update Contenedores.py

--- YMS/Contenedores/Contenedores.py
+++ YMS/Contenedores/Contenedores.py
@@ -514,7 +514,7 @@ def Tipo_Nuevo_1(Datos):
                 if str(Datos["Nivel_1"]) == "Outbound":
                     Formulario = {"Col":"12", "Campos": [],"Clase": "Asignar" }
                     Formulario["Campos"].append({"tipo":"archivo","campo":"Fotos","titulo":"Photos","Requerido":1,"Col":12,"min":1,"max":5,"tipo_archivo":["image/*"],"valor":""})
-                    Formulario["Campos"].append({"tipo":"checkbox","campo":"Salida","titulo":"it ready departure?","Requerido":0,"valor":False,"editable":False})
+                    Formulario["Campos"].append({"tipo":"checkbox","campo":"Salida","titulo":"it ready departure?","Requerido":0,"valor":False,"editable":True})
                     Resultado["Contenido"] += str(Compartido_2023.Formulario(Formulario))
                     Resultado["Contenido"] += """
                     <div id='info_salida'></div>
